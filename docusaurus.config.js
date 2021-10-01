6d537859615b7702d8f09224f4ed25d558a202c2
Deyan Vitanov <deyan.vitanov@gmail.com>
2021-09-30 21:49:58 -0700

    testing a search solution

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,4 +105,21 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         darkTheme: darkCodeTheme,
       },
     }),
+
+  plugins: [
+    // ... Your other plugins.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+        // When applying `zh` in language, please install `nodejieba` in your project.
+      },
+    ],
+  ],
 });
